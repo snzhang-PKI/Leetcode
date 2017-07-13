c25fc3fabdb4a34f44334fe93b4c0c9fc0011f96
Songnan Zhang <songnan.zhang@perkinelmer.com>
2017-07-13 10:45:18 -0400

    7/13

---
+++ 290.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+
+
+int main(int argc, char const *argv[])
+{
+	unordered_map <int,char> m;
+	cout<<m[1]<<endl;
+	return 0;
+}
